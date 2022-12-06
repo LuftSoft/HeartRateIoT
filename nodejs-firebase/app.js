bfd9ba8101f84743bcd272810ccea10891e699bb
TanNgoc <factyel.bttn@gmail.com>
2022-12-06 21:27:51 +0700

    change from realtime website

--- nodejs-firebase/app.js
+++ nodejs-firebase/app.js
@@ -1,4 +1,4 @@
-import pkg from 'express'
+import pkg, { response } from 'express'
 const express = pkg;
 // import { BodyParser } from "body-parser";
 // import { cors } from "cors";
@@ -6,11 +6,24 @@ const express = pkg;
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue } from "firebase/database";
+import * as csv from "csv";
+import * as fs from "fs";
+import * as parser from 'csv-parse'
+import * as xl from "xlsx";
+import * as ejs from "ejs";
+import path, { dirname } from 'path';
+import { fileURLToPath } from "url";
+import axios, { isCancel, AxiosError } from 'axios'
+import fetch from "node-fetch";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const API_KEY = 'api_key=d9754441dd4cfa2e7edac814a34b3c96'
+const BASE_URL = 'https://api.themoviedb.org/3'
 const firebaseConfig = {
     apiKey: "AIzaSyB9kGz6N1ZSRnMNjbslbAmUVpFTmJjR5Ec",
     authDomain: "iot2022-cad90.firebaseapp.com",
@@ -23,21 +36,50 @@ const firebaseConfig = {
 };
 
 
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(firebaseApp);
 const starCountRef = ref(db, 'heart/realtime');
-onValue(starCountRef, (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-    // updateStarCount(postElement, data);
-});
+// onValue(starCountRef, (snapshot) => {
+//     const data = snapshot.val();
+//     console.log(data);
+//     console.log(new Date(data.time));
+// });
+// updateStarCount(postElement, data); 
 //const analytics = getAnalytics(firebaseApp);
 
+
 const app = express();
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+// app.engine('ejs', ejs)
+var num = 0;
+setTimeout(() => {
+    num++;
+}, 1000);
+var mData = '';
+app.get('/', async (req, res) => {
+    res.render('index', { num: num })
+});
 
+app.get('/movie-api', async (req, res) => {
+    fetch('https://api.themoviedb.org/3/movie/550?api_key=d9754441dd4cfa2e7edac814a34b3c96')
+        .then(res => res.json())
+        .then(function (data) {
+            mData = data
+            console.log(data);
+            res.send(data.overview)
+        })
+})
+app.get('/api/get-heart-beat', async (req, res) => {
 
-
+    onValue(starCountRef, async (snapshot) => {
+        app.locals.data = await snapshot.val();
+    });
+    res.json({ data: app.locals.data })
+})
+app.use(express.static(__dirname + '/public'))
 app.listen(3222, () => {
     console.log("Open port 3222");
 })
@@ -50,3 +92,71 @@ Cần làm
     Tại 1 giao diện nhỏ hiển thị nhịp tim realtime.
     https://firebase.google.com/docs/reference/node/firebase.database.DataSnapshot?hl=en&authuser=0
 */
+/*
+var records = []
+
+var result = []
+fs.createReadStream('./film.csv')
+    .pipe(parser.parse({
+        columns: true
+    }))
+    .on('data', data => {
+        let tmp = {}
+        tmp.Title = data.Title;
+        tmp.Plot = data.Plot;
+        tmp.Director = data.Director;
+        tmp.Genre = data.Genre;
+        tmp.Cast = data.Cast;
+        //console.log(data)
+        records.push(tmp);
+    })
+    .on('error', error => {
+        console.log(error);
+    })
+    .on('end', () => {
+        for (let film of records) {
+            console.log(String(film.Plot).split(" ").length)
+            let plot = String(film.Plot);
+            let header;
+            let sPlot;
+            try {
+                header = plot.split(".")[0] + plot.split(".")[1]
+                sPlot = plot.substring(plot.split(".")[0].length + plot.split(".")[1].length + 1);
+                sPlot = sPlot.trim();
+            } catch (error) {
+                header = plot.split(".")[0]
+                sPlot = plot.substring(plot.split(".")[0].length + 1);
+                sPlot = sPlot.trim();
+            }
+            result.push({ Movie: film.Title, Input: header });
+            let cnt = 0; let len = 0;
+            for (let i = 0; i < 14; i++) {
+                try {
+                    len += sPlot.split(" ")[cnt * 2].length + sPlot.split(" ")[cnt * 2 + 1].length
+                    result.push({ Movie: film.Title, Input: header + sPlot.substring(0, len) });
+                    cnt++;
+                } catch (error) {
+                    result.push({ Movie: film.Title, Input: header + sPlot.substring(0, len) });
+                }
+            }
+            if ((typeof (film.Director) !== 'undefined') && (String(film.Director).toLowerCase() !== 'unknown') && (String(film.Director).trim() !== '')) { result.push({ Movie: film.Title, Input: String(film.Director) }); }
+            else { result.push({ Movie: film.Title, Input: '0' }); }
+            if ((typeof (film.Genre) !== 'undefined') && (String(film.Genre).toLowerCase() !== 'unknown') && (String(film.Genre).trim() !== '')) { result.push({ Movie: film.Title, Input: String(film.Genre) }); }
+            else { result.push({ Movie: film.Title, Input: '0' }); }
+            if ((typeof (film.Cast) !== 'undefined') && (String(film.Cast).toLowerCase() !== 'unknown') && (String(film.Cast).trim() !== '')) { result.push({ Movie: film.Title, Input: String(film.Cast) }); }
+            else { result.push({ Movie: film.Title, Input: '0' }); }
+            result.push({ Movie: film.Title, Input: '0' });
+            result.push({ Movie: film.Title, Input: '0' });
+            //console.log(result);
+        }
+        const worksheet = xl.utils.json_to_sheet(result)
+        const workBook = xl.utils.book_new();
+
+        xl.utils.book_append_sheet(workBook, worksheet, "records");
+        xl.write(workBook, { bookType: "xlsx", type: 'binary' })
+        xl.writeFile(workBook, "mrecord.xlsx")
+        console.log("end reading");
+    })
+
+ */
+
---
+++ nodejs-firebase/test.md
@@ -0,0 +1,40 @@
+API: movieDb (en-US // vi)
+url ảnh : https://image.tmdb.org/t/p/original
+url ảnh to: https://image.tmdb.org/t/p/w500
+
+Genres - Lấy thể loaị
+phim: 
+https://api.themoviedb.org/3/genre/movie/list?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US
+tv show:
+https://api.themoviedb.org/3/genre/tv/list?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US
+
+Keyword - Tìm kiếm theo từ khoá
+https://api.themoviedb.org/3/search/keyword?api_key=e9e9d8da18ae29fc430845952232787c&page=1&query=women
+
+Chi tiết của phim
+https://api.themoviedb.org/3/movie/297762?api_key=e9e9d8da18ae29fc430845952232787c&append_to_response=videos
+
+ Review của phim
+https://api.themoviedb.org/3/movie/297762/reviews?api_key=e9e9d8da18ae29fc430845952232787c
+
+ Danh sách phim tương tự của phim
+https://api.themoviedb.org/3/movie/297762/similar?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1
+
+ Danh sách diễn viên của phim
+http://api.themoviedb.org/3/movie/297762/casts?api_key=e9e9d8da18ae29fc430845952232787c
+
+Home
+https://api.themoviedb.org/3/movie/now_playing?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1  
+https://api.themoviedb.org/3/movie/popular?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1 
+https://api.themoviedb.org/3/movie/upcoming?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1 
+https://api.themoviedb.org/3/movie/top_rated?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1
+
+      
+
+
+ 
+Get Account
+	https://api.themoviedb.org/3/account?api_key=e9e9d8da18ae29fc430845952232787c
+Api thêm vào danh sách yêu thích
+POST:
+Api lấy danh sách phim yêu thích
