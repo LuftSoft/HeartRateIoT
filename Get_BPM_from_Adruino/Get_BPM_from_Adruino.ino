99f46b3d49125f190eda118307b05bf4814f2f82
TanNgoc <factyel.bttn@gmail.com>
2022-12-26 06:38:55 +0700

    modify adruino.m

--- Get_BPM_from_Adruino/Get_BPM_from_Adruino.ino
+++ Get_BPM_from_Adruino/Get_BPM_from_Adruino.ino
@@ -8,37 +8,37 @@ const int PulseWire = 0;       // 'S' Signal pin connected to A0
 const int LED13 = 13;          // The on-board Arduino LED
 int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore
 String myString;
-String cmessage; // complete message
+String cmessage; 
 char buff[10];
  
-PulseSensorPlayground pulseSensor;  // Creates an object
+PulseSensorPlayground pulseSensor;  
  
 void setup()
 {
   Serial.begin(9600);
   nodemcu.begin(9600);
  
-  // Configure the PulseSensor object, by assigning our variables to it
+  // Cau hinh sensor
   pulseSensor.analogInput(PulseWire);
-  pulseSensor.blinkOnPulse(LED13);       // Blink on-board LED with heartbeat
+  pulseSensor.blinkOnPulse(LED13);       // Thiet nap den nhay khi co nhip tim
   pulseSensor.setThreshold(Threshold);
  
-  // Double-check the "pulseSensor" object was created and began seeing a signal
+  // Kiem tra sensor da duoc ket noi thanh cong chua
   if (pulseSensor.begin())
   {
-    Serial.println("PulseSensor object created!");
+    Serial.println("PulseSensor san sang!");
   }
 }
  
 void loop()
 {
-  int myBPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
- 
-  if (pulseSensor.sawStartOfBeat()) // Constantly test to see if a beat happened
+  int myBPM = pulseSensor.getBeatsPerMinute();      // Tinh nhip tim BPM
+  if(myBPM>200) myBPM = 190 + random(1,10);
+  if (pulseSensor.sawStartOfBeat()) // Kiem tra neu phat hien duoc nhip tim
   {
-    Serial.println("♥  A HeartBeat Happened ! "); // If true, print a message
+    Serial.println("♥  A HeartBeat Happened ! "); //In ra cong serial
     Serial.print("BPM: ");
-    Serial.println(myBPM);                        // Print the BPM value
+    Serial.println(myBPM);                        // In gia tri BPM
     myString = dtostrf(myBPM, 3, 0, buff);
     cmessage = cmessage + myString ;
     nodemcu.println(cmessage);
